f99c7c8fa54cd57aaaaa13fbd6bfb12e665401c7
vpasport <pasportnikov2000@gmail.com>
2022-11-12 21:45:10 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a> <br/> 
-          TSET CI/CD
+          TEST CI/CD
         </h1>
 
         <p className={styles.description}>
