7552bf6f325ec1e4800b6adfc9ce1f4f3dfd4720
Anna Lozovaya <anyalozovaya2014@gmail.com>
2022-11-14 23:10:54 +0300

    feat: change type button

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,7 +32,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <Button>I'm a button</Button>
+        <Button type="border">I'm a button</Button>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
