b52fbb5887d06890095188d37ff3e6fd5bd7415c
vpasport <pasportnikov2000@gmail.com>
2022-11-18 22:45:28 +0300

    fix husky

--- pages/components/index.jsx
+++ pages/components/index.jsx
@@ -2,16 +2,20 @@ import { Blocks } from '@components';
 
 import styles from './style.module.scss';
 
-const TestPaage = () => (
-  <div className={styles.root}>
-    <Blocks.IslandBlock />
-    <Blocks.NotificationsBlock />
-    <Blocks.ButtonsBlock />
-    <Blocks.PopupBlock />
-    <Blocks.InputsBlock />
-    <Blocks.TextEditorBlock />
-    <Blocks.LoaderBlock />
-  </div>
-);
+const TestPaage = () => {
+  console.log(1);
+
+  return (
+    <div className={styles.root}>
+      <Blocks.IslandBlock />
+      <Blocks.NotificationsBlock />
+      <Blocks.ButtonsBlock />
+      <Blocks.PopupBlock />
+      <Blocks.InputsBlock />
+      <Blocks.TextEditorBlock />
+      <Blocks.LoaderBlock />
+    </div>
+  );
+};
 
 export default TestPaage;
