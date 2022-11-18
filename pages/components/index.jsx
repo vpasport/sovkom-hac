8746e2c042f660740c3adb8a1b948c50c87b0ca5
vpasport <pasportnikov2000@gmail.com>
2022-11-18 23:03:09 +0300

    fix husky

--- pages/components/index.jsx
+++ pages/components/index.jsx
@@ -3,9 +3,6 @@ import { Blocks } from '@components';
 import styles from './style.module.scss';
 
 const TestPaage = () => {
-  console.log(1);
-  console.log(2);
-
   return (
     <div className={styles.root}>
       <Blocks.IslandBlock />
