c8ebe9e2eb08823dc4438ba24433111492ec5403
vpasport <pasportnikov2000@gmail.com>
2022-11-20 02:43:32 +0300

    fix

--- pages/user/account/[id]/index.jsx
+++ pages/user/account/[id]/index.jsx
@@ -1,3 +1,42 @@
-const Account = () => 'account';
+// // import { useState } from 'react';
+
+// import { checkUser } from '@middlewares';
+// import * as CurrencyService from '@api/currency';
+// // import * as UserService from '@api/user';
+
+// // import styles from './style.module.scss';
+
+// const Account = () => 'test';
+
+// export const getServerSideProps = (ctx) =>
+//   checkUser(
+//     ctx,
+//     async ({ user }) => {
+//       try {
+//         const currency = await (await CurrencyService.getAvailable()).data;
+//         // const history = await (await UserService.)
+
+//         return {
+//           props: {
+//             user,
+//             currency: currency.filter((el) => !el.banned),
+//           },
+//         };
+//       } catch (e) {
+//         console.error(e);
+//         return {
+//           redirect: {
+//             destination: '/user',
+//             permanent: true,
+//           },
+//         };
+//       }
+//     },
+//     { redirectToLogin: true },
+//   );
+
+// export default Account;
+
+const Account = () => 'test';
 
 export default Account;
